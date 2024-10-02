527f7d70d7fd1a2387c23cefff59660f600c0a8b
André Teixeira <andre-x8@hotmail.com>
2024-10-02 19:59:52 -0300

    add conexao com banco

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
 
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+connection
+  .authenticate()
+  .then(() => {
+    console.log("conexão com banco feita com sucesso");
+  })
+  .catch((error) => {
+    console.log("erro na conexao com o banco ", error);
+  });
 
 app.get("/", (req, res) => {
   res.render("index"); //pagina inicial
