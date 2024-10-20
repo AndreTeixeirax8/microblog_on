8057137c42ac0fbdcc826f98646bbd3f64308083
André Teixeira <andre-x8@hotmail.com>
2024-10-20 15:19:55 -0300

    add  configuracao de artigos

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const categoriesController = require("./categories/CategoriesController");
 const categoriesArticles = require("./articles/ArticlesController");
 const Article = require("./articles/Article");
 const Category = require("./categories/Category");
+const { where } = require("sequelize");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,6 +29,25 @@ app.get("/", (req, res) => {
   Article.findAll().then((articles) => {
     res.render("index", { articles: articles }); //pagina inicial
   });
+});
+
+app.get("/:slug", (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug,
+    },
+  })
+    .then((article) => {
+      if (article != undefined) {
+        res.render("article", { article: article });
+      } else {
+        res.redirect("/");
+      }
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
 });
 
 app.listen(3000, () => {
